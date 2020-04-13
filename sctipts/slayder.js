8215410c6cc80025c1f6525d83e5633007635388
VitaliSantalau <v.santalau@gmail.com>
2020-04-13 18:19:46 +0300

    slide ready

--- sctipts/slayder.js
+++ sctipts/slayder.js
@@ -1,60 +1,68 @@
 let wrapper = document.querySelector(".slide_wrapper");
 let slideItems = document.querySelectorAll('.slide_item');
 let slideControl = document.querySelectorAll('.slide_control');
-let leftArrow = document.querySelector('.leftArrow');
-let rightArrow = document.querySelector('.rightArrow');
 
 let step = 100; // one slide visible
+
 // variable for moving all slides together
 let wrapperMovingIndex = 0;
 let wrapperMoving = wrapperMovingIndex * step;
+
 // variable for moving slide 
 const minSlideIndex = 0;
 const maxSlideIndex = slideItems.length - 1;
 let slideIndex = 0;
 let slideMoving = slideItems.length * step;
-let slideMovingCycle = 0;
-let slideMovingLenghtWay; 
+
 // array of slides
 let slides = [];
 slideItems.forEach(function(slide, index) {
   slides.push({
     slide: slide,
-    position: index,
     transform: 0 
   })
 });
 
 function slidesToLeft() {
+  wrapperMovingIndex--;
+  wrapperMoving = wrapperMovingIndex*step;
+  wrapper.style.transform = `translateX(${wrapperMoving}%)`;
+};
+
+function slidesToRight() {
+  wrapperMovingIndex++;
+  wrapperMoving = wrapperMovingIndex*step;
+  wrapper.style.transform = `translateX(${wrapperMoving}%)`;
+};
+
+function slideToEnd() {
+  if(slideIndex !== maxSlideIndex) {
+    slides[maxSlideIndex].slide.style.zIndex = '2';
+  } else {
+    slides[maxSlideIndex].slide.style.zIndex = '1';
+    };
+  slides[slideIndex].slide.style.cssText = "transition: transform 0.1s ease 2s";
+  slides[slideIndex].transform = slides[slideIndex].transform + slideMoving;
+  slides[slideIndex].slide.style.transform = `translateX(${slides[slideIndex].transform}%)`;
   slideIndex++;
   if(slideIndex > maxSlideIndex) {
     slideIndex = minSlideIndex;
   }
-  console.log(slideIndex);
-  /////////////
-  wrapperMovingIndex--;
-  wrapperMoving = wrapperMovingIndex*step;
-  wrapper.style.transform = `translateX(${wrapperMoving}%)`;
-}
-
-function slideToEnd() {
-    
-}
+};
 
 function slideToStart() {
-  
-}
-
-function slidesToRight() {
   slideIndex--;
-  if(slideIndex<minSlideIndex) {
+  if(slideIndex < minSlideIndex) {
     slideIndex = maxSlideIndex;
   }
-  console.log(slideIndex);
-  ////////////////
-  wrapperMovingIndex++;
-  wrapperMoving = wrapperMovingIndex*step;
-  wrapper.style.transform = `translateX(${wrapperMoving}%)`;
+  if(slideIndex == minSlideIndex) {
+    slides[minSlideIndex].slide.style.zIndex = '3';
+  } else {
+    slides[minSlideIndex].slide.style.zIndex = '2';
+    };
+  slides[slideIndex].transform = slides[slideIndex].transform - slideMoving;
+  slides[slideIndex].slide.style.cssText = "transition: transform 0.1s ease";
+  slides[slideIndex].slide.style.transform = `translateX(${slides[slideIndex].transform}%)`;
 }
 
 slideControl.forEach(arrow => {
@@ -63,9 +71,8 @@ slideControl.forEach(arrow => {
       slidesToLeft();
       slideToEnd();
     } else {
-        slidesToRight();
-      }
+      slideToStart();
+      slidesToRight();
+      };
   }
 });
-
-
